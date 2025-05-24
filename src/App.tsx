179530fc56548fd79b0356e07a5215eef18fe91e
Byakoxx <helmerenriquetorres@gmail.com>
2025-05-24 15:19:33 -0500

    refactor: migrate project to TypeScript and add Tailwind CSS
    Migrate the project from JavaScript to TypeScript to improve type safety and maintainability. Add Tailwind CSS for styling and configure it with PostCSS and Vite. Remove old JavaScript files and update dependencies accordingly.

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-2xl font-bold text-blue-500">Tailwind is working!</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
